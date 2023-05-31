ab45fe6c3b5e17c1b976cc3219f73aeb3aae96e3
RafalPelc <rafal.pelc2211@gmail.com>
2023-05-31 11:50:20 +0200

    HW7

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+
+createGalleryItems();
+
+function createGalleryItems() {
+  for (const galleryItem of galleryItems) {
+    const galleryLi = document.createElement("li");
+    galleryLi.classList.add("gallery__item");
+    gallery.append(galleryLi);
+
+    const galleryLink = document.createElement("a");
+    galleryLink.classList.add("gallery__link");
+    galleryLink.href = galleryItem.original;
+    galleryLi.append(galleryLink);
+
+    const galleryImage = document.createElement("img");
+    galleryImage.classList.add("gallery__image");
+    galleryImage.src = galleryItem.preview;
+    galleryImage.alt = galleryItem.description;
+    galleryImage.dataset.source = galleryItem.original;
+    galleryLink.append(galleryImage);
+  }
+}
+
+const openImage = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImage = event.target.dataset.source;
+  const modal = basicLightbox.create(`
+<div class="modal">
+<img src="${selectedImage}" width="800" height="600">
+</div>
+`);
+  modal.show();
+
+  const keyClose = (event) => {
+    if (event.key === "Escape" && modal.visible) {
+      modal.close();
+    }
+  };
+  document.addEventListener("keydown", keyClose);
+};
+
+gallery.addEventListener("click", openImage);
+console.log(galleryItems);
